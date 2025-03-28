b4eeb6dd478bb66d2173ffce2a8416d0e541cf58
wa1tnr <wa1tnr@users.noreply.github.com>
2025-03-28 13:08:32 +0000

    UNTESTED basal file

--- elevator-aa/src/main.cpp
+++ elevator-aa/src/main.cpp
@@ -1,1 +1,12 @@
-Fri 28 Mar 13:07:23 UTC 2025
+/* NUCLEO  L031K6 */
+/* elevator 28 March 2025 */
+
+/* Fri 28 Mar 13:05:39 UTC 2025 */
+
+#include <Arduino.h>
+
+void setup() { ; }
+
+void loop() { ; }
+
+/* end. */
