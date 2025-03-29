bbfc32cafe9db69da2a2482fb5373cd35fd91986
wa1tnr <wa1tnr@users.noreply.github.com>
2025-03-29 11:26:12 +0000

    Mostly reformatting to conform to wokwi
      wokwi online editor env prefers indents of 2 sp
    
    	modified:   main.cpp
    
    On branch dvlp-aa

--- elevator-aa/src/main.cpp
+++ elevator-aa/src/main.cpp
@@ -1,7 +1,10 @@
+/* nsketch_elvtr-aa.cpp */
+
 /* NUCLEO  L031K6 */
+
 /* elevator 28 March 2025 */
 
-/* Fri 28 Mar 17:46 41 UTC 2025 */
+/* Fri 28 Mar 20:37 25 UTC 2025 */
 
 #include <Arduino.h>
 #include <stdint.h>
@@ -13,23 +16,23 @@ unsigned long millisBB;
 const unsigned long duration = 2000;
 
 void reporting() {
-    ;
+  ;
 }
 
 bool reading(uint8_t pin) {
-    bool r = digitalRead(pin);
-    r = !r; // change to positive logic asap
-    if (r) {
-        delay(300);
-    }
-    return r;
+  bool r = digitalRead(pin);
+  r = !r; // change to positive logic asap
+  if (r) {
+    delay(300);
+  }
+  return r;
 }
 
 void sayPinMsg(uint8_t pin) {
-    Serial.write(' ');
-    Serial.write('r');
-    Serial.write(' ');
-    Serial.print(pin);
+  Serial.write(' ');
+  Serial.write('r');
+  Serial.write(' ');
+  Serial.print(pin);
 }
 
 const uint8_t pbsw[] = {15, 15, 16, 17, 18, 19};
@@ -37,53 +40,87 @@ const uint8_t pbsw[] = {15, 15, 16, 17, 18, 19};
 /* const uint8_t led[] = { 1, 1, 2, 3, 4, 5 }; // don't really need a map at all
  */
 
+bool ledNState = 0;
+
+void ledNOnTime() {
+  delay(700);
+}
+
+void ledNOffTime() {
+  delay(1400);
+}
+
+void blink(uint8_t ledN) {
+  ledNState = !ledNState;
+  digitalWrite(ledN, ledNState);
+  ledNOnTime();
+
+  ledNState = !ledNState;
+  digitalWrite(ledN, ledNState);
+  ledNOffTime();
+}
+
+void blinks(uint8_t ledN) {
+  pinMode(ledN, OUTPUT);
+  for (uint8_t repeat = 3; repeat > 0; repeat--) {
+    blink(ledN);
+  }
+}
+
+void ledDemoAA() {
+  for (uint8_t ledN = 1; ledN < 6; ledN++) {
+    blinks(ledN);
+  }
+}
+
 void scanPin(uint8_t index) {
-    uint8_t pin = pbsw[index];
-    bool r = reading(pin);
-    if (r) {
-        sayPinMsg(pin);
-    }
+  uint8_t pin = pbsw[index];
+  bool r = reading(pin);
+  if (r) {
+    sayPinMsg(pin);
+  }
 }
 
 void runLoop() {
-    for (uint8_t index = 1; index < 6; index++) {
-        scanPin(index);
-    }
+  for (uint8_t index = 1; index < 6; index++) {
+    scanPin(index);
+  }
 }
 
 void printCR() {
-    Serial.println("");
+  Serial.println("");
 }
 
 void sayPorts() {
-    Serial.println("  D15, 16, 17, 18 and 19 for A1..A5");
+  Serial.println("  D15, 16, 17, 18 and 19 for A1..A5");
 }
 
 void initSerial() {
-    Serial.begin(9600);
-    Serial.println("    program begins.");
-    printCR();
-    sayPorts();
-    printCR();
+  Serial.begin(9600);
+  Serial.println("    program begins.");
+  printCR();
+  sayPorts();
+  printCR();
 }
 
 void initGPIO() {
-    Serial.println("initGPIO() now: ");
-    for (uint8_t index = 1; index < 6; index++) {
-        pinMode(pbsw[index], INPUT_PULLUP);
-        pinMode(index, OUTPUT); // a bit jank but coincidentally does 'work'
-    }
+  Serial.println("initGPIO() now: ");
+  for (uint8_t index = 1; index < 6; index++) {
+    pinMode(pbsw[index], INPUT_PULLUP);
+    pinMode(index, OUTPUT); // a bit jank but coincidentally does 'work'
+  }
+  ledDemoAA();
 }
 
 void setup() {
-    delay(7400);
-    initSerial();
-    initGPIO();
+  delay(7400);
+  initSerial();
+  initGPIO();
 }
 
 void loop() {
-    runLoop();
-    reporting();
+  runLoop();
+  reporting();
 }
 
 /* end. */
