b07fed289e3510330a8a8fe7ff87890bbb3c48f2
wa1tnr <wa1tnr@users.noreply.github.com>
2025-03-30 15:08:28 +0000

    blind commit recent work

--- elevator-aa/src/main.cpp
+++ elevator-aa/src/main.cpp
@@ -60,10 +60,18 @@ void blink(uint8_t ledN) {
   ledNOffTime();
 }
 
+void blinkToggle(uint8_t ledNT) {
+  digitalToggle(ledNT);
+  ledNOnTime();
+  digitalToggle(ledNT);
+  ledNOffTime();
+}
+
 void blinks(uint8_t ledN) {
   pinMode(ledN, OUTPUT);
   for (uint8_t repeat = 3; repeat > 0; repeat--) {
-    blink(ledN);
+    // blink(ledN);
+    blinkToggle(ledN);
   }
 }
 
@@ -71,6 +79,9 @@ void ledDemoAA() {
   for (uint8_t ledN = 1; ledN < 6; ledN++) {
     blinks(ledN);
   }
+  blinks(7); // PC14 was 23
+  blinks(8); // PC15 was 24
+  Serial.println(" ledDemoAA() now complete.");
 }
 
 void scanPin(uint8_t index) {
@@ -93,6 +104,7 @@ void printCR() {
 
 void sayPorts() {
   Serial.println("  D15, 16, 17, 18 and 19 for A1..A5");
+  Serial.println("  29 March 11:55z:  D23 and D24 for D7 and D8 experiment: ");
 }
 
 void initSerial() {
@@ -103,12 +115,21 @@ void initSerial() {
   printCR();
 }
 
+void initOutlierGPIOs() {
+  Serial.println("initOutlierGPIOs() now: ");
+  pinMode(7, OUTPUT); // PC14 tried 23
+  pinMode(8, OUTPUT); // PC15 tried 24
+  /* ref. */
+  /* https://github.com/stm32duino/Arduino_Core_STM32/blob/main/variants/STM32L0xx/L031K(4-6)T_L041K6T/variant_generic.h */
+}
+
 void initGPIO() {
   Serial.println("initGPIO() now: ");
   for (uint8_t index = 1; index < 6; index++) {
     pinMode(pbsw[index], INPUT_PULLUP);
     pinMode(index, OUTPUT); // a bit jank but coincidentally does 'work'
   }
+  initOutlierGPIOs();
   ledDemoAA();
 }
 
